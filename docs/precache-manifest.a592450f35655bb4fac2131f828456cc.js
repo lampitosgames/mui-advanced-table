a114569fb543a93b8a2bd57bc75083a152317054
Daniel Timko <dtimko@callibrity.com>
2019-10-25 10:52:40 -0400

    Removed leading / from urls in manifest files

--- docs/precache-manifest.a592450f35655bb4fac2131f828456cc.js
+++ docs/precache-manifest.a592450f35655bb4fac2131f828456cc.js
@@ -1,26 +1,25 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
+self.__precacheManifest = (self.__precacheManifest || []).concat([{
     "revision": "c1184e4834cffdf649ab19a66c5fb464",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "6bda1fbb0e069eac7809",
-    "url": "/static/css/main.c13a0d33.chunk.css"
+    "url": "static/css/main.c13a0d33.chunk.css"
   },
   {
     "revision": "c6b65586639aa1bb3219",
-    "url": "/static/js/2.2a94ae84.chunk.js"
+    "url": "static/js/2.2a94ae84.chunk.js"
   },
   {
     "revision": "6bda1fbb0e069eac7809",
-    "url": "/static/js/main.e88e193d.chunk.js"
+    "url": "static/js/main.e88e193d.chunk.js"
   },
   {
     "revision": "211094691a8619894090",
-    "url": "/static/js/runtime-main.c9faf01f.js"
+    "url": "static/js/runtime-main.c9faf01f.js"
   },
   {
     "revision": "25bf045ca257e971124f3997d89f321c",
-    "url": "/static/media/logo.25bf045c.svg"
+    "url": "static/media/logo.25bf045c.svg"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a592450f35655bb4fac2131f828456cc.js"
+  "precache-manifest.a592450f35655bb4fac2131f828456cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
+
+  blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
 });
